0a407b275f4db363c342cc92bec550de00534a5d
Danh Nguyen <DanhNguyen5@proton.me>
2023-05-31 06:48:50 -0500

    added experience, started contact.js

--- src/data/data.js
+++ src/data/data.js
@@ -17,3 +17,16 @@ export const MyTechnologies = [
   "Microsoft Office Suite",
 ];
 
+export const Exp = [
+  {
+    summary: "Experienced in building and optimizing desktops, including unusual configurations like a headless cryptocurrency mining workstation, while also possessing hardware repair skills and a passion for staying up-to-date with the latest computer hardware innovations.",
+    title : "Computer Hardware Fabrication",
+    
+  },
+  {
+    summary: "Collaborated with a real-world company to fulfill a design request involving recycling solid waste fibers into a woven cloth for a usable product, taking the lead in drafting a conveyor belt using engineering techniques, computer-aided drafting software, and control system software. Fabricated the parts using machining and 3D printing, and presented the entire machine blueprint and project outcome to the client and fellow engineers.",
+    title: "Mechanical Engineer Project"
+  },
+];
+
+
