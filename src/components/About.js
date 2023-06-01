0e89a06b8292cf0bd6043364e4ab6d1491a5a98d
Danh Nguyen <DanhNguyen5@proton.me>
2023-06-01 18:15:17 -0500

    code review

--- src/components/About.js
+++ src/components/About.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import AnimatedText from './AnimatedText';
-
+// hero banner section
 const About = () => {
   return (
     <div id="aboutme">
-      <div className="container mx-auto flex pt-10 pb-20 sm:flex-row flex-col items-center justify-center bg-customColor" style={{ margin: '0 auto', maxWidth: '100%' }}>
+      {/*container class is not defined in Tailwind, mx-auto, margins are adjusted left and right to center itself in the parent */}
+      {/* pt - means add 10 pixels of padding to the top, pb - means add 20 pixels of padding to bottm */}
+      {/* items-center - items will be vertically centered */}
+      {/* justify-center items will be horizontally centered */}
+      <div className="container mx-auto flex sm:flex-row flex-col pt-10 pb-20  items-center justify-center bg-customColor" style={{ margin: '0 auto', maxWidth: '100%' }}>
         <div className="flex flex-wrap w-full sm:w-1/3 text-2xl">
           <AnimatedText />
         </div>
